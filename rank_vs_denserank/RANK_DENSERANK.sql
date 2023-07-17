14fe87dbd4b4339c72e8f982807bc63a183da057
niteshspandey <93583756+niteshspandey@users.noreply.github.com>
2023-07-17 16:49:25 +0530

    Update RANK_DENSERANK.sql

--- rank_vs_denserank/RANK_DENSERANK.sql
+++ rank_vs_denserank/RANK_DENSERANK.sql
@@ -23,3 +23,16 @@ SELECT
     )
 FROM
     emp;
+
+
+select * from t_emp_det order by sal desc
+
+Nitesh	1	23456	20
+Nitesh	100	23000	10
+Sam	111	23000	30
+Sarvesh	110	20000	20
+Purnima	130	15000	10
+Tara	131	15000	10
+
+select dense_rank(15000) within group(order by sal desc) rnk from t_emp_det;--4
+select rank(15000) within group(order by sal desc) rnk from t_emp_det;--5
